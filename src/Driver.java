16c42db6622d42bd021db384e7ed68c8dd358742
Antonio Franchini <antonio.franchini@garmin.com>
2018-11-08 14:38:48 -0700

    Completed ArraysAndStrings

--- src/Driver.java
+++ src/Driver.java
@@ -6,44 +6,8 @@ public class Driver {
 
     public static void main(String args[]){
 
-        Graph l = new Graph();
-
-        l.addVertex("A");
-        l.addVertex("B");
-        l.addVertex("C");
-        l.addVertex("D");
-        l.addVertex("E");
-        l.addVertex("F");
-        l.addVertex("G");
-        l.addVertex("H");
-        l.addVertex("K");
-        l.addVertex("M");
-
-        l.addEdge("A", "B");
-        l.addEdge("A", "C", 80);
-        l.addEdge("A", "D", 5);
-        l.addEdge("B", "C", 2);
-        l.addEdge("B", "F");
-        l.addEdge("C", "D");
-        l.addEdge("F", "K");
-        l.addEdge("F", "E");
-        l.addEdge("E", "G");
-        l.addEdge("E", "H");
-        l.addEdge("G", "M");
-        l.addEdge("H", "M");
-
-        //        This test graph looks like this:
-        //
-        //        A ---- B ---- F ---- E ---- G
-        //        | \  / 2      |      |      |
-        //        |   /         |      |      |
-        //     80 |  /\         |      |      |
-        //        | /  \ 5       |      |      |
-        //        C ---- D      K      H ---- M
-
-        l.BreadthFirstSearch("H");
-
-        System.out.println( "\nDistance = " + l.dijkstras( "A", "D" ) );
+        ArraysAndStrings a = new ArraysAndStrings();
+        System.out.println( "" + a.isRotation( "casagrande", "agrandecas" ) );
     }
 
 }
--- src/ctci/ArraysAndStrings.java
+++ src/ctci/ArraysAndStrings.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class ArraysAndStrings {
 
     /***************************************************************************************************************************
@@ -6,8 +8,16 @@ public class ArraysAndStrings {
      What if you can not use additional data structures?
      ***************************************************************************************************************************/
 
-    public boolean uniqueChars( String in[] ){
-        return false;
+    public boolean uniqueChars( String s ){
+        final int NUM_ASCII_CHARS = 256;
+        int counts[] = new int[ NUM_ASCII_CHARS ];
+        for( int i = 0; i < s.length(); i++ ){
+            counts[ (int)s.charAt( i ) ]++;
+            if( counts[ (int)s.charAt( i ) ] > 1 ){
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -17,8 +27,26 @@ public class ArraysAndStrings {
      including the null character.)
      ***************************************************************************************************************************/
 
-    public String reverseString( String in[] ){
-        return "out";
+    public String reverseString( String s ){
+        for( int i = 0; i < ( s.length()-1 ) / 2; i++ ){
+            s = swapLetters( s, i, s.length()-2-i );
+        }
+        return s;
+    }
+
+    public String swapLetters( String s, int idx1, int idx2 ){
+        if( s.length()-1 < idx1 || s.length()-1 < idx2 ){
+            return s;
+        }
+        String outString = "";
+        int first  = ( idx1 < idx2 ) ? idx1 : idx2;
+        int second = ( first == idx1 ) ? idx2 : idx1;
+
+        outString += s.substring( 0, first );
+        outString += s.charAt( second ) +  s.substring( first+1, second ) + s.charAt( first );
+            outString += s.substring( second+1, s.length() );
+
+        return outString;
     }
 
 
@@ -30,8 +58,17 @@ public class ArraysAndStrings {
      FOLLOW UP: Write the test cases for this method
      ***************************************************************************************************************************/
 
-    public String removeDuplicates( String in[] ){
-        return "out";
+    public String removeDuplicates( String s ){
+        final int NUM_ASCII_CHARS = 256;
+        int counts[] = new int[ NUM_ASCII_CHARS ];
+        for( int i = 0; i < s.length(); i++ ){
+            counts[ (int)s.charAt( i ) ]++;
+            if( counts[ (int)s.charAt( i ) ] > 1 ){
+                s = s.substring( 0, i ) + s.substring( i + 1, s.length() );
+                i--;
+            }
+        }
+        return s;
     }
 
 
@@ -40,8 +77,29 @@ public class ArraysAndStrings {
      Write a method to decide if two strings are anagrams or not.
      ***************************************************************************************************************************/
 
-    public boolean areAnagrams( String a[], String b[] ){
-        return false;
+    public boolean areAnagrams( String a, String b ){
+        if( a.length() != b.length() ){
+            return false;
+        }
+
+        final int NUM_ASCII_CHARS = 256;
+        int countsA[] = new int[ NUM_ASCII_CHARS ];
+        int countsB[] = new int[ NUM_ASCII_CHARS ];
+        int n = a.length();
+
+        for( int i = 0; i < n; i++ ){
+            countsA[ (int)a.charAt( i ) ]++;
+        }
+        for( int i = 0; i < n; i++ ){
+            countsB[ (int)b.charAt( i ) ]++;
+        }
+
+        for( int i = 0; i < NUM_ASCII_CHARS; i++ ){
+            if( countsA[ i ] != countsB[ i ] ){
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -50,8 +108,14 @@ public class ArraysAndStrings {
      Write a method to replace all spaces in a string with ‘%20’.
      ***************************************************************************************************************************/
 
-    public String replaceSpaces( String in[] ){
-        return "out";
+    public String replaceSpaces( String s ){
+        for( int i = 0; i < s.length(); i++ ){
+            if( s.charAt( i ) == ' ' ){
+                s = s.substring( 0, i ) + "%20" + s.substring( i + 1, s.length() );
+                i =+ 2;
+            }
+        }
+        return s;
     }
 
 
@@ -61,8 +125,20 @@ public class ArraysAndStrings {
      write a method to rotate the image by 90 degrees. Can you do this in place?
      ***************************************************************************************************************************/
 
-    public boolean rotateMatrixby90( int in[][] ){
-        return false;
+    public void rotateMatrixby90( int matrix[][] ){
+        if( matrix.length != matrix[ 0 ].length ){
+            return;
+        }
+        int n = matrix.length -1;
+        for( int loop = 0; loop < n / 2; loop++ ){
+            for( int offset = loop; offset < n - loop; offset++ ) {
+                int temp                           = matrix[ offset     ][ loop       ];
+                matrix[ offset     ][ loop       ] = matrix[ n - loop   ][ offset     ];
+                matrix[ n - loop   ][ offset     ] = matrix[ n - offset ][ n - loop   ];
+                matrix[ n - offset ][ n - loop   ] = matrix[ loop       ][ n - offset ];
+                matrix[ loop       ][ n - offset ] = temp;
+            }
+        }
     }
 
 
@@ -71,7 +147,29 @@ public class ArraysAndStrings {
      Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0.
      ***************************************************************************************************************************/
 
-    public void zeroOut( int in[][] ){
+    public void zeroOut( int matrix[][] ){
+        Vector<Integer> rowsToZero = new Vector<>();
+        Vector<Integer> colsToZero = new Vector<>();
+
+        for( int i = 0; i < matrix.length; i++ ){
+            for( int j = 0; j < matrix[ 0 ].length; j++ ){
+                if( matrix[ i ][ j ] == 0 ){
+                    rowsToZero.add( i );
+                    colsToZero.add( j );
+                }
+            }
+        }
+
+        for( int i = 0; i < rowsToZero.size(); i++ ){
+            for( int j = 0; j < matrix[ 0 ].length; j++ ){
+                matrix[ rowsToZero.get( i ) ][ j ] = 0;
+            }
+        }
+        for( int i = 0; i < colsToZero.size(); i++ ){
+            for( int j = 0; j < matrix.length; j++ ){
+                matrix[ j ][ colsToZero.get( i ) ] = 0;
+            }
+        }
 
     }
 
@@ -83,11 +181,33 @@ public class ArraysAndStrings {
      (i.e., “waterbottle” is a rotation of “erbottlewat”).
      ***************************************************************************************************************************/
 
-    public boolean isRotation( String a[], String b[] ){
+    public boolean isRotation( String s1, String s2 ){
+        if( s1.length() != s2.length() ){
+            return false;
+        }
+
+        String doubleCopy = s1 + s1;
+
+        if( isSubstring( s2, doubleCopy ) ){
+            return true;
+        }
         return false;
     }
 
-    public boolean isSubstring( String a[], String b[] ){
+    public boolean isSubstring( String a, String b ){
+        if( a.length() > b.length() ){
+            return false;
+        }
+
+        for( int i = 0; i < b.length() - a.length(); i++ ){
+            int j = 0;
+            while( j < a.length() && b.charAt( i + j ) == a.charAt( j ) ){
+                j++;
+            }
+            if( j == a.length() ){
+                return true;
+            }
+        }
         return false;
     }
 
